babf69210fdecfb096800a816138e9c731c608ab
Fernando Pérez Lara <fernando.perez.lara@outlook.com>
2023-05-24 16:13:01 +0200

    test: undo test clang linter

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,7 +3,8 @@
 #include "windows/MainWindow/MainWindow.h"
 
 int main(int argc, char* argv[]) {
-  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create("com.smartknob.simulator");
+  Glib::RefPtr<Gtk::Application> app =
+      Gtk::Application::create("com.smartknob.simulator");
 
   return app->make_window_and_run<MainWindow>(argc, argv);
 }
