9592ce6acde6093805e6fe05aa72c6a6efae99fd
Fernando Perez <fernando.perez.lara@outlook.com>
2023-06-09 11:26:53 +0200

    feat: add style example to MainWindow

--- src/MainWindow/MainWindow.cpp
+++ src/MainWindow/MainWindow.cpp
@@ -6,8 +6,13 @@ MainWindow::MainWindow(BaseObjectType* cobject,
     m_refBuilder(refBuilder) {
   auto menu_builder = Gtk::Builder::create_from_resource(
       "/com/smartknob/simulator/MainWindow/Menu.ui");
-  auto menu = menu_builder->get_object<Gio::MenuModel>("menu");
+  
+  auto css_provider = Gtk::CssProvider::create();
+  css_provider->load_from_path("src/MainWindow/style.css");
+  auto display = Gdk::Display::get_default();
+  Gtk::StyleContext::add_provider_for_display(display, css_provider, GTK_STYLE_PROVIDER_PRIORITY_USER);
 
+  auto menu = menu_builder->get_object<Gio::MenuModel>("menu");
   m_gears = m_refBuilder->get_widget<Gtk::MenuButton>("gears");
   m_gears->set_menu_model(menu);
 
@@ -26,9 +31,7 @@ MainWindow* MainWindow::create() {
 
 void MainWindow::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width,
                          int height) {
-  const double radius = 100.0;
-
   cr->set_source_rgb(0.0, 0.0, 0.0);
-  cr->arc(width / 2, height / 2, radius, 0.0, 2.0 * M_PI);
+  cr->arc(150, 150, 120, 0.0, 2.0 * M_PI);
   cr->fill();
 }
