98b378d73e885cf07160cc2bb9a03fe667c6d9bc
Fernando Perez <fernando.perez.lara@outlook.com>
2023-06-09 12:37:13 +0200

    feat: add slider

--- src/MainWindow/MainWindow.cpp
+++ src/MainWindow/MainWindow.cpp
@@ -4,9 +4,6 @@ MainWindow::MainWindow(BaseObjectType* cobject,
                        const Glib::RefPtr<Gtk::Builder>& refBuilder) :
     Gtk::ApplicationWindow(cobject),
     m_refBuilder(refBuilder) {
-  auto menu_builder = Gtk::Builder::create_from_resource(
-      "/com/smartknob/simulator/MainWindow/Menu.ui");
-
   auto css_provider = Gtk::CssProvider::create();
   css_provider->load_from_resource(
       "/com/smartknob/simulator/MainWindow/style.css");
@@ -14,7 +11,7 @@ MainWindow::MainWindow(BaseObjectType* cobject,
   Gtk::StyleContext::add_provider_for_display(display, css_provider,
                                               GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-  auto menu = menu_builder->get_object<Gio::MenuModel>("menu");
+  auto menu = m_refBuilder->get_object<Gio::MenuModel>("menu");
   m_gears = m_refBuilder->get_widget<Gtk::MenuButton>("gears");
   m_gears->set_menu_model(menu);
 
@@ -33,7 +30,12 @@ MainWindow* MainWindow::create() {
 
 void MainWindow::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width,
                          int height) {
+  (void) width;
+  (void) height;
+
+  double size = 120;
+
   cr->set_source_rgb(0.0, 0.0, 0.0);
-  cr->arc(150, 150, 120, 0.0, 2.0 * M_PI);
+  cr->arc(size, size, size, 0.0, 2.0 * M_PI);
   cr->fill();
 }
